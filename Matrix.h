a63301c367907547edeca3449fbc0b7ea8766a50
Ulf Benjaminsson <ulfben@gmail.com>
2023-02-06 17:52:49 +0100

    Simplifying template arguments. Adjusting help messages (asserts)

--- Matrix.h
+++ Matrix.h
@@ -3,7 +3,8 @@
 #include "Tuple.h"
 
 template <uint8_t ROWS, uint8_t COLUMNS> struct Matrix {
-    static_assert((uint16_t(ROWS)* COLUMNS) < std::numeric_limits<uint8_t>::max(), "Matrix is limited to 255 elements.");
+    static_assert(ROWS > 0 && COLUMNS > 0 && "Matrix dimensions must be non-zero.");
+    static_assert((uint16_t(ROWS) * COLUMNS) < std::numeric_limits<uint8_t>::max(), "Matrix is limited to 255 elements.");
     using value_type = Real;
     using pointer = value_type*;
     using const_pointer = const value_type*;
@@ -12,6 +13,7 @@ template <uint8_t ROWS, uint8_t COLUMNS> struct Matrix {
     using size_type = uint8_t;
     using reference = value_type&;
     using const_reference = const value_type&;
+    using submatrix_type = Matrix<ROWS - 1, COLUMNS - 1>;
     static constexpr size_type HEIGHT = ROWS;
     static constexpr size_type WIDTH = COLUMNS;
 
@@ -44,12 +46,12 @@ template <uint8_t ROWS, uint8_t COLUMNS> struct Matrix {
     }
         
     constexpr auto submatrix(size_type remove_row, size_type remove_column) const noexcept {
-        assert(remove_row < rows() && remove_column < columns() && "submatrix: row and column to remove must be inside the original matrix.");
-        Matrix<ROWS - 1, COLUMNS - 1> r;          
-        uint8_t ri = 0;
+        assert(remove_row < rows() && remove_column < columns() && "Matrix::submatrix() arguments are out of range. row and column must be inside the input matrix.");
+        submatrix_type r;          
+        size_type ri = 0;
         for (size_type i = 0; i < size(); i++) {
-            //if(index_to_row(i) == remove_row) { i += columns(); } //skip the entire row
-            if(index_to_row(i) != remove_row && index_to_column(i) != remove_column) {
+            if(index_to_row(i) == remove_row) { i += columns(); } //skip the entire row
+            if(index_to_column(i) != remove_column) {
                 r[ri++] = _data[i];
             }
         }     
@@ -91,47 +93,44 @@ constexpr bool operator==(const Matrix& lhs, const Matrix& rhs) noexcept {
 }
 
 constexpr Matrix4 operator*(const Matrix4& lhs, const Matrix4& rhs) noexcept {    
-    Matrix4 result;
-    result(0, 0) = lhs(0, 0) * rhs(0, 0) + lhs(0, 1) * rhs(1, 0) + lhs(0, 2) * rhs(2, 0) + lhs(0, 3) * rhs(3, 0);
-    result(0, 1) = lhs(0, 0) * rhs(0, 1) + lhs(0, 1) * rhs(1, 1) + lhs(0, 2) * rhs(2, 1) + lhs(0, 3) * rhs(3, 1);
-    result(0, 2) = lhs(0, 0) * rhs(0, 2) + lhs(0, 1) * rhs(1, 2) + lhs(0, 2) * rhs(2, 2) + lhs(0, 3) * rhs(3, 2);
-    result(0, 3) = lhs(0, 0) * rhs(0, 3) + lhs(0, 1) * rhs(1, 3) + lhs(0, 2) * rhs(2, 3) + lhs(0, 3) * rhs(3, 3);
-    result(1, 0) = lhs(1, 0) * rhs(0, 0) + lhs(1, 1) * rhs(1, 0) + lhs(1, 2) * rhs(2, 0) + lhs(1, 3) * rhs(3, 0);
-    result(1, 1) = lhs(1, 0) * rhs(0, 1) + lhs(1, 1) * rhs(1, 1) + lhs(1, 2) * rhs(2, 1) + lhs(1, 3) * rhs(3, 1);
-    result(1, 2) = lhs(1, 0) * rhs(0, 2) + lhs(1, 1) * rhs(1, 2) + lhs(1, 2) * rhs(2, 2) + lhs(1, 3) * rhs(3, 2);
-    result(1, 3) = lhs(1, 0) * rhs(0, 3) + lhs(1, 1) * rhs(1, 3) + lhs(1, 2) * rhs(2, 3) + lhs(1, 3) * rhs(3, 3);
-    result(2, 0) = lhs(2, 0) * rhs(0, 0) + lhs(2, 1) * rhs(1, 0) + lhs(2, 2) * rhs(2, 0) + lhs(2, 3) * rhs(3, 0);
-    result(2, 1) = lhs(2, 0) * rhs(0, 1) + lhs(2, 1) * rhs(1, 1) + lhs(2, 2) * rhs(2, 1) + lhs(2, 3) * rhs(3, 1);
-    result(2, 2) = lhs(2, 0) * rhs(0, 2) + lhs(2, 1) * rhs(1, 2) + lhs(2, 2) * rhs(2, 2) + lhs(2, 3) * rhs(3, 2);
-    result(2, 3) = lhs(2, 0) * rhs(0, 3) + lhs(2, 1) * rhs(1, 3) + lhs(2, 2) * rhs(2, 3) + lhs(2, 3) * rhs(3, 3);
-    result(3, 0) = lhs(3, 0) * rhs(0, 0) + lhs(3, 1) * rhs(1, 0) + lhs(3, 2) * rhs(2, 0) + lhs(3, 3) * rhs(3, 0);
-    result(3, 1) = lhs(3, 0) * rhs(0, 1) + lhs(3, 1) * rhs(1, 1) + lhs(3, 2) * rhs(2, 1) + lhs(3, 3) * rhs(3, 1);
-    result(3, 2) = lhs(3, 0) * rhs(0, 2) + lhs(3, 1) * rhs(1, 2) + lhs(3, 2) * rhs(2, 2) + lhs(3, 3) * rhs(3, 2);
-    result(3, 3) = lhs(3, 0) * rhs(0, 3) + lhs(3, 1) * rhs(1, 3) + lhs(3, 2) * rhs(2, 3) + lhs(3, 3) * rhs(3, 3);
-    return result;
+    return Matrix4{
+    lhs(0, 0) * rhs(0, 0) + lhs(0, 1) * rhs(1, 0) + lhs(0, 2) * rhs(2, 0) + lhs(0, 3) * rhs(3, 0), //0,0
+    lhs(0, 0) * rhs(0, 1) + lhs(0, 1) * rhs(1, 1) + lhs(0, 2) * rhs(2, 1) + lhs(0, 3) * rhs(3, 1), //0,1
+    lhs(0, 0) * rhs(0, 2) + lhs(0, 1) * rhs(1, 2) + lhs(0, 2) * rhs(2, 2) + lhs(0, 3) * rhs(3, 2), //0,2 
+    lhs(0, 0) * rhs(0, 3) + lhs(0, 1) * rhs(1, 3) + lhs(0, 2) * rhs(2, 3) + lhs(0, 3) * rhs(3, 3), //0,3
+    lhs(1, 0) * rhs(0, 0) + lhs(1, 1) * rhs(1, 0) + lhs(1, 2) * rhs(2, 0) + lhs(1, 3) * rhs(3, 0),
+    lhs(1, 0) * rhs(0, 1) + lhs(1, 1) * rhs(1, 1) + lhs(1, 2) * rhs(2, 1) + lhs(1, 3) * rhs(3, 1),
+    lhs(1, 0) * rhs(0, 2) + lhs(1, 1) * rhs(1, 2) + lhs(1, 2) * rhs(2, 2) + lhs(1, 3) * rhs(3, 2),
+    lhs(1, 0) * rhs(0, 3) + lhs(1, 1) * rhs(1, 3) + lhs(1, 2) * rhs(2, 3) + lhs(1, 3) * rhs(3, 3),
+    lhs(2, 0) * rhs(0, 0) + lhs(2, 1) * rhs(1, 0) + lhs(2, 2) * rhs(2, 0) + lhs(2, 3) * rhs(3, 0),
+    lhs(2, 0) * rhs(0, 1) + lhs(2, 1) * rhs(1, 1) + lhs(2, 2) * rhs(2, 1) + lhs(2, 3) * rhs(3, 1),
+    lhs(2, 0) * rhs(0, 2) + lhs(2, 1) * rhs(1, 2) + lhs(2, 2) * rhs(2, 2) + lhs(2, 3) * rhs(3, 2),
+    lhs(2, 0) * rhs(0, 3) + lhs(2, 1) * rhs(1, 3) + lhs(2, 2) * rhs(2, 3) + lhs(2, 3) * rhs(3, 3),
+    lhs(3, 0) * rhs(0, 0) + lhs(3, 1) * rhs(1, 0) + lhs(3, 2) * rhs(2, 0) + lhs(3, 3) * rhs(3, 0), //3,0
+    lhs(3, 0) * rhs(0, 1) + lhs(3, 1) * rhs(1, 1) + lhs(3, 2) * rhs(2, 1) + lhs(3, 3) * rhs(3, 1), //3,1
+    lhs(3, 0) * rhs(0, 2) + lhs(3, 1) * rhs(1, 2) + lhs(3, 2) * rhs(2, 2) + lhs(3, 3) * rhs(3, 2), //3,2
+    lhs(3, 0) * rhs(0, 3) + lhs(3, 1) * rhs(1, 3) + lhs(3, 2) * rhs(2, 3) + lhs(3, 3) * rhs(3, 3)};//3,3
 }
 
 constexpr Matrix3 operator*(const Matrix3& lhs, const Matrix3& rhs) noexcept {    
-    Matrix3 result;
-    result(0, 0) = lhs(0, 0) * rhs(0, 0) + lhs(0, 1) * rhs(1, 0) + lhs(0, 2) * rhs(2, 0);
-    result(0, 1) = lhs(0, 0) * rhs(0, 1) + lhs(0, 1) * rhs(1, 1) + lhs(0, 2) * rhs(2, 1);
-    result(0, 2) = lhs(0, 0) * rhs(0, 2) + lhs(0, 1) * rhs(1, 2) + lhs(0, 2) * rhs(2, 2);
-    result(1, 0) = lhs(1, 0) * rhs(0, 0) + lhs(1, 1) * rhs(1, 0) + lhs(1, 2) * rhs(2, 0);
-    result(1, 1) = lhs(1, 0) * rhs(0, 1) + lhs(1, 1) * rhs(1, 1) + lhs(1, 2) * rhs(2, 1);
-    result(1, 2) = lhs(1, 0) * rhs(0, 2) + lhs(1, 1) * rhs(1, 2) + lhs(1, 2) * rhs(2, 2);
-    result(2, 0) = lhs(2, 0) * rhs(0, 0) + lhs(2, 1) * rhs(1, 0) + lhs(2, 2) * rhs(2, 0);
-    result(2, 1) = lhs(2, 0) * rhs(0, 1) + lhs(2, 1) * rhs(1, 1) + lhs(2, 2) * rhs(2, 1);
-    result(2, 2) = lhs(2, 0) * rhs(0, 2) + lhs(2, 1) * rhs(1, 2) + lhs(2, 2) * rhs(2, 2);
-    return result;
+    return Matrix3{
+    lhs(0, 0) * rhs(0, 0) + lhs(0, 1) * rhs(1, 0) + lhs(0, 2) * rhs(2, 0), //(0, 0)
+    lhs(0, 0) * rhs(0, 1) + lhs(0, 1) * rhs(1, 1) + lhs(0, 2) * rhs(2, 1), //(0, 1)
+    lhs(0, 0) * rhs(0, 2) + lhs(0, 1) * rhs(1, 2) + lhs(0, 2) * rhs(2, 2), //(0, 2)
+    lhs(1, 0) * rhs(0, 0) + lhs(1, 1) * rhs(1, 0) + lhs(1, 2) * rhs(2, 0),
+    lhs(1, 0) * rhs(0, 1) + lhs(1, 1) * rhs(1, 1) + lhs(1, 2) * rhs(2, 1),
+    lhs(1, 0) * rhs(0, 2) + lhs(1, 1) * rhs(1, 2) + lhs(1, 2) * rhs(2, 2),
+    lhs(2, 0) * rhs(0, 0) + lhs(2, 1) * rhs(1, 0) + lhs(2, 2) * rhs(2, 0), //(2, 0)
+    lhs(2, 0) * rhs(0, 1) + lhs(2, 1) * rhs(1, 1) + lhs(2, 2) * rhs(2, 1), //(2, 1)
+    lhs(2, 0) * rhs(0, 2) + lhs(2, 1) * rhs(1, 2) + lhs(2, 2) * rhs(2, 2)};//(2, 2)    
 }
 
 constexpr Matrix2 operator*(const Matrix2& lhs, const Matrix2& rhs) noexcept {    
-    Matrix2 result;    
-    result(0, 0) = lhs(0, 0) * rhs(0, 0) + lhs(0, 1) * rhs(1, 0);
-    result(0, 1) = lhs(0, 0) * rhs(0, 1) + lhs(0, 1) * rhs(1, 1);
-    result(1, 0) = lhs(1, 0) * rhs(0, 0) + lhs(1, 1) * rhs(1, 0);
-    result(1, 1) = lhs(1, 0) * rhs(0, 1) + lhs(1, 1) * rhs(1, 1);
-    return result;
+    return Matrix2{
+    lhs(0, 0) * rhs(0, 0) + lhs(0, 1) * rhs(1, 0), //(0, 0)
+    lhs(0, 0) * rhs(0, 1) + lhs(0, 1) * rhs(1, 1), //(0, 1)
+    lhs(1, 0) * rhs(0, 0) + lhs(1, 1) * rhs(1, 0), //(1, 0)
+    lhs(1, 0) * rhs(0, 1) + lhs(1, 1) * rhs(1, 1)};//(1, 1)    
 }
 
 //multiplying arbitrarily sized (square) Matrices.
@@ -157,11 +156,12 @@ Tuple operator*(const Matrix4& lhs, const Tuple rhs) noexcept {
     return Tuple{ x, y, z, w };
 }
 
-template <uint8_t ROWS, uint8_t COLUMNS>
-Matrix<ROWS, COLUMNS> transpose(const Matrix<ROWS, COLUMNS>& a) noexcept {
-    Matrix<ROWS, COLUMNS> result;
-    for (uint8_t row = 0; row < ROWS; ++row) {
-        for (uint8_t col = 0; col < COLUMNS; ++col) {
+template <class Matrix>
+Matrix transpose(const Matrix& a) noexcept {
+    using size_type = Matrix::size_type;
+    Matrix result;
+    for (size_type row = 0; row < Matrix::HEIGHT; ++row) {
+        for (size_type col = 0; col < Matrix::WIDTH; ++col) {
             result(col, row) = a(row, col);
         }
     }
@@ -203,43 +203,44 @@ constexpr Real determinant(const Matrix4& m) noexcept {
 }
 
 //calculate determinant on arbitrary Matrix sizes.
-template <uint8_t ROWS, uint8_t COLUMNS>
-constexpr Real determinant2(const Matrix<ROWS, COLUMNS>& m) noexcept {        
-    using size_type = Matrix<ROWS, COLUMNS>::size_type;
+template <class Matrix>
+constexpr Real determinant2(const Matrix& m) noexcept {        
+    using size_type = Matrix::size_type;
     Real out = 0;
-    for (size_type col = 0; col < COLUMNS; col++) {
+    for (size_type col = 0; col < Matrix::WIDTH; col++) {
         out += m(0, col) * cofactor(m, 0, col);
     }
     return out;
 }
 
-template <uint8_t ROWS, uint8_t COLUMNS>
-constexpr auto submatrix(const Matrix<ROWS, COLUMNS>& in, uint8_t remove_row, uint8_t remove_column) noexcept {
-    assert(remove_row < ROWS&& remove_column < COLUMNS && "invalid submatrix specification. row and column must be inside the input matrix.");
-    using size_type = Matrix<ROWS, COLUMNS>::size_type;
-    Matrix<ROWS - 1, COLUMNS - 1> out;
+template <class Matrix>
+constexpr auto submatrix(const Matrix& in, uint8_t remove_row, uint8_t remove_column) noexcept {
+    assert(remove_row < Matrix::HEIGHT && remove_column < Matrix::WIDTH && "submatrix() arguments are out of range. row and column must be inside the input matrix.");
+    using size_type = Matrix::size_type;
+    using SubMatrix = Matrix::submatrix_type;    
+    SubMatrix out;
     size_type ri = 0;
-    for (size_type i = 0; i < ROWS; i++) {
+    for (size_type i = 0; i < Matrix::HEIGHT; i++) {
         if (i == remove_row) { continue; }
-        for (size_type j = 0; j < COLUMNS; j++) {
+        for (size_type j = 0; j < Matrix::WIDTH; j++) {
             if (j == remove_column) { continue; }
-            out[ri++] = in[i * COLUMNS + j];
+            out[ri++] = in[i * Matrix::WIDTH + j];
         }
     }
     return out;
 }
 
 //Returns the determinant of a submatrix
-template <uint8_t ROWS, uint8_t COLUMNS>
-constexpr Real minor(const Matrix<ROWS, COLUMNS>& in, uint8_t remove_row, uint8_t remove_column) noexcept {
-    assert(remove_row < ROWS&& remove_column < COLUMNS && "invalid submatrix specification. row and column must be inside the input matrix.");
+template <class Matrix>
+constexpr Real minor(const Matrix& in, uint8_t remove_row, uint8_t remove_column) noexcept {
+    assert(remove_row < Matrix::HEIGHT && remove_column < Matrix::WIDTH && "minor() arguments are out of range. row and column must be inside the input matrix.");
     const auto sub = submatrix(in, remove_row, remove_column);
     return determinant(sub);
 }
 
-template <uint8_t ROWS, uint8_t COLUMNS>
-constexpr Real cofactor(const Matrix<ROWS, COLUMNS>& in, uint8_t remove_row, uint8_t remove_column) noexcept {
-    assert(remove_row < ROWS&& remove_column < COLUMNS && "invalid submatrix specification. row and column must be inside the input matrix.");
+template <class Matrix>
+constexpr Real cofactor(const Matrix& in, uint8_t remove_row, uint8_t remove_column) noexcept {
+    assert(remove_row < Matrix::HEIGHT && remove_column < Matrix::WIDTH && "cofactor() arguments are out of range. row and column must be inside the input matrix.");
     const auto min = minor(in, remove_row, remove_column);
     return math::is_odd(remove_row + remove_column) ? -min : min;
 }
