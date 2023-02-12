3ec6f3a88852a3a78b300c0ea4f58c9fb93876f2
Ulf Benjaminsson <ulfben@gmail.com>
2023-02-12 11:11:38 +0100

    Add ray-sphere intersections

--- Ray.h
+++ Ray.h
@@ -4,6 +4,49 @@
 struct Ray;
 constexpr Point position(const Ray& r, Real time) noexcept;
 
+struct Sphere {
+    Point position; 
+    Real radius;
+};
+
+constexpr Sphere sphere(Point p, Real radius) noexcept {
+    return Sphere{ p, radius };
+}
+
+struct Intersection {
+    using size_type = uint8_t;
+    using value_type = Real;
+    using reference = value_type&;
+    size_type count{0};
+    value_type xs[2];
+    
+    constexpr value_type operator[](size_type i) const noexcept {
+        assert(i < count && "Intersection::operator[i] index is out of bounds");
+        return xs[i];
+    }
+    constexpr reference operator[](size_type i) noexcept {
+        assert(i < count && "Intersection::operator[i] index is out of bounds");
+        return xs[i];
+    }
+};
+
+struct Intersections {
+    using size_type = Intersection::size_type;
+    using value_type = Intersection;
+    using reference = value_type&;
+    size_type count{0};
+    value_type xs[2];
+    
+    constexpr value_type operator[](size_type i) const noexcept {
+        assert(i < count && "Intersections::operator[i] index is out of bounds");
+        return xs[i];
+    }
+    constexpr reference operator[](size_type i) noexcept {
+        assert(i < count && "Intersections::operator[i] index is out of bounds");
+        return xs[i];
+    }
+};
+
 struct Ray {
     Point origin;
     Vector direction;
@@ -21,3 +64,17 @@ constexpr Ray ray(Point p, Vector dir) noexcept {
 constexpr Point position(const Ray& r, Real time) noexcept {    
     return r.origin + r.direction * time;
 }
+
+constexpr Intersection intersect(const Sphere& s, const Ray& r) noexcept {
+    const Vector sphere_to_ray = r.origin - s.position;
+    const auto a = dot(r.direction, r.direction);
+    const auto b = 2 * dot(r.direction, sphere_to_ray);
+    const auto c = dot(sphere_to_ray, sphere_to_ray) - 1;
+    const auto discriminant = (b * b) - (4 * a * c);
+    if (discriminant < 0) {
+        return Intersection{};
+    }
+    const auto t1 = (-b - std::sqrt(discriminant)) / (2*a);
+    const auto t2 = (-b + std::sqrt(discriminant)) / (2*a);
+    return Intersection{ 2, t1, t2 };
+};
--- tests/RayTests.h
+++ tests/RayTests.h
@@ -2,6 +2,8 @@
 #include "../pch.h"
 #include "../Ray.h"
 
+
+
 TEST(Ray, CanBeConstructed) {
     const auto origin = point(1, 2, 3);
     const auto direction = vector(4,5,6);
@@ -22,4 +24,52 @@ TEST(Ray, canComputePointFromDistance) {
     EXPECT_EQ(r.position(1), point(3,3,4));
     EXPECT_EQ(r.position(-1), point(1,3,4));
     EXPECT_EQ(r.position(2.5f), point(4.5,3,4));
+}
+
+TEST(Ray, intersectSphereAtTwoPoints) {
+    const auto r = ray(point(0,0,-5), vector(0,0,1));    
+    const auto s = sphere(point(0,0,0), 1.0f);
+    const auto xs = intersect(s, r);
+
+    EXPECT_EQ(xs.count, 2);
+    EXPECT_FLOAT_EQ(xs[0], 4.0f);
+    EXPECT_FLOAT_EQ(xs[1], 6.0f);
+}
+
+TEST(Ray, intersectSphereAtATangent) {
+    const auto r = ray(point(0,1,-5), vector(0,0,1));    
+    const auto s = sphere(point(0,0,0), 1.0f);
+    const auto xs = intersect(s, r);
+
+    EXPECT_EQ(xs.count, 2);
+    EXPECT_FLOAT_EQ(xs[0], 5.0f);
+    EXPECT_FLOAT_EQ(xs[1], 5.0f);
+}
+
+TEST(Ray, intersectMissingASphere) {
+    const auto r = ray(point(0,2,-5), vector(0,0,1));    
+    const auto s = sphere(point(0,0,0), 1.0f);
+    const auto xs = intersect(s, r);
+
+    EXPECT_EQ(xs.count, 0);
+}
+
+TEST(Ray, intersectWhenOriginInsideSphere) {
+    const auto r = ray(point(0,0,0), vector(0,0,1));    
+    const auto s = sphere(point(0,0,0), 1.0f);
+    const auto xs = intersect(s, r);
+
+    EXPECT_EQ(xs.count, 2);
+    EXPECT_FLOAT_EQ(xs[0], -1.0f); //ray extends backwards! critical for reflections. 
+    EXPECT_FLOAT_EQ(xs[1], 1.0f);
+}
+
+TEST(Ray, intersectWhenSphereBehindRay) {
+    const auto r = ray(point(0,0,5), vector(0,0,1));    
+    const auto s = sphere(point(0,0,0), 1.0f);
+    const auto xs = intersect(s, r);
+
+    EXPECT_EQ(xs.count, 2);
+    EXPECT_FLOAT_EQ(xs[0], -6.0f); //ray extends backwards! critical for reflections. 
+    EXPECT_FLOAT_EQ(xs[1], -4.0f);
 }
