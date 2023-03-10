295e53390c11abfb95c943e7f8bc7ed15e649173
Ulf Benjaminsson <ulfben@gmail.com>
2023-03-10 16:04:15 +0100

    refactor pattern implementation (from Pattern<std::function> to std::variant<Patterns>) Add operators to pattern Add Pattern to Material

--- Material.h
+++ Material.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "pch.h"
 #include "Tuple.h"
+#include "Pattern.h"
 
 struct Material final {
     Color color{ 1, 1, 1 };
     Real ambient = 0.1f;
-    Real diffuse = 0.9f; 
+    Real diffuse = 0.9f;
     Real specular = 0.9f;
     Real shininess = 200.0f;
     Real reflective = 0.0f;
     Real transparency = 0.0f;
     Real refractive_index = 0.0f;
+    Patterns pattern = null_pattern();
     constexpr bool operator==(const Material& that) const noexcept = default;
 };
 #pragma warning(push)
@@ -36,4 +38,9 @@ constexpr Material glass(Real refractive = 1.52f) noexcept {
     m.transparency = 1.0f;
     m.refractive_index = refractive;
     return m;
+}
+
+constexpr bool has_pattern(const Material& mat) noexcept {
+    return std::visit([](const auto& obj) noexcept -> bool { return static_cast<bool>(obj); }, mat.pattern);
+
 }
--- Pattern.h
+++ Pattern.h
@@ -3,21 +3,51 @@
 #include "pch.h"
 #include "Tuple.h"
 
-struct Pattern final {
-    std::function<Color(const Point& p)> apply_pattern;
-
-    Color at(const Point& p) const noexcept {
-        return apply_pattern(p);
-    }
+struct NullPattern final {
+    constexpr Color at([[maybe_unused]] const Point& p) const noexcept { return BLACK; }
+    explicit constexpr operator bool() const  { return false; }
+    constexpr bool empty() const  { return true; }
+    constexpr bool operator==(const NullPattern& that) const noexcept = default;
 };
 
-auto strip_pattern(Color a, Color b) noexcept {
-    const auto func = [a, b](const Point& p) noexcept {        
+struct StripedPattern final {
+    Color a{};
+    Color b{};
+    constexpr Color at(const Point& p) const noexcept {
         return (math::int_floor(p.x) % 2 == 0) ? a : b;
-    };
-    return Pattern{func};
+    }
+    explicit constexpr operator bool() const  { return true; }
+    constexpr bool empty() const  { return false; }
+    constexpr bool operator==(const StripedPattern& that) const noexcept  = default;
+    
 };
 
-Color pattern_at(const Pattern& pattern, const Point& p) noexcept {
-    return pattern.at(p);
+using Patterns = std::variant<NullPattern, StripedPattern>; 
+template<typename T> 
+concept patterns = std::is_same_v<NullPattern, T> || std::is_same_v<StripedPattern, T>;
+
+template<typename T>
+requires patterns<T>
+inline bool operator==(const T& t, const Patterns& v) {
+    const T* c = std::get_if<T>(&v);
+    return c && *c == t; // true if v contains a pattern that compares equal to v
+}
+template<typename T>
+requires patterns<T>
+inline bool operator==(const Patterns& v, const T& t) {
+    return t == v;
+}
+
+
+constexpr auto null_pattern() noexcept {
+    return NullPattern{};
+};
+
+constexpr auto strip_pattern(Color a, Color b) noexcept {
+    return StripedPattern{ a, b };
+};
+
+Color pattern_at(const Patterns& variant, const Point& p) noexcept {
+    assert(!std::holds_alternative<NullPattern>(variant) && "pattern_at: called on NullPattern.");
+    return std::visit([&p](const auto& obj) -> Color { return obj.at(p); }, variant);
 };
