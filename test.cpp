370418d245b8c2cc772c96ccd627d6ef77239e8e
Ulf Benjaminsson <ulfben@gmail.com>
2023-04-03 14:57:13 +0200

    Adding rapidyaml.h courtesy of https://github.com/biojppm/rapidyaml

--- test.cpp
+++ test.cpp
@@ -5,6 +5,9 @@
 #include "Matrix.h"
 #include "Shapes.h"
 
+#define RYML_SINGLE_HDR_DEFINE_NOW
+#include "external/rapidyaml.h"
+
 #include "tests/CanvasTests.h"
 #include "tests/FloatCompareTests.h"
 #include "tests/ColorTests.h"
@@ -474,4 +477,23 @@ TEST(DISABLED_Bonus01, CanTextureMap) {
                               light);    
     const auto img = render(c, world);
     save_to_file(img, "output/bonus_chapter_texture_01.ppm"sv);
+}
+
+TEST(DISABLED_Bonus01, CanCubeMap) {        
+    const auto c = Camera(400, 400, 0.5f, 
+        view_transform(point(0.0f, 0.0f, -5.0f), point(0, 0, 0), vector(0, 1, 0)));
+    const auto light = point_light(point(-10, 10, -10), color(1, 1, 1));
+    
+    const auto green = color_from_srgb(0, 0.5f, 0);
+    const auto texture = texture_map(uv_checkers(20, 10, green, WHITE), spherical_map);    
+    Material mat = material(texture);
+    mat.ambient = 0.1f;
+    mat.specular = 0.4f;
+    mat.shininess = 10.0f;
+    mat.diffuse = 0.6f;
+    const auto s = sphere(mat);
+    const auto world = World({s}, 
+                              light);    
+    const auto img = render(c, world);
+    save_to_file(img, "output/bonus_chapter_texture_02.ppm"sv);
 }
